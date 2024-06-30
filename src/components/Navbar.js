d5e54fe4debf10d6c5a8b4d7cb41ea154ae74bf9
M S Devanarayan <116883606+msdnarayan@users.noreply.github.com>
2024-06-30 17:36:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const handleNewChatClick = (e) => {
+    e.preventDefault(); // Prevent the default anchor behavior
+    window.location.reload(); // Reload the page
+  };
+
   return (
     <div className="navbar">
-      <a href="#home">New Chat</a>
-      <div >
-      <a href='#home' className='aboutus'>AboutUs</a>
-    </div>
+      <a href="#home" onClick={handleNewChatClick}>New Chat</a>
+      <div>
+        <a href="#home" className="aboutus">AboutUs</a>
+      </div>
     </div>
   );
 };
