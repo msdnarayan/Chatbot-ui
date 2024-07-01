0dc4621865dff919a1314ea5c79c2f7519405bc5
M S Devanarayan <116883606+msdnarayan@users.noreply.github.com>
2024-07-01 21:07:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,36 +2,78 @@ import React, { useState } from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox';
 import ChatInput from './components/ChatInput';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import ChatMessage from './components/ChatMessage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import AboutUs from './components/AboutUs';
+import { Link } from 'react-router-dom';
+import InfoIcon from '@mui/icons-material/Info';
+import AddCommentIcon from '@mui/icons-material/AddComment';
 
-function App() {
+
+
+function Chat() {
   const [messages, setMessages] = useState([]);
+  const [isVisible, setIsVisible] = useState(true);
 
   const addMessage = (message, sender) => {
-    setMessages(prevMessages => [...prevMessages, { text: message, sender }]);
+    setMessages(prevMessages => [...prevMessages, { text: message, sender, feedback: '' }]);
   };
-  const [isVisible, setIsVisible] = useState(true);
+
+  const addFeedback = (index, feedback) => {
+    setMessages(prevMessages =>
+      prevMessages.map((msg, i) =>
+        i === index ? { ...msg, feedback } : msg
+      )
+    );
+  };
 
   const handleSendClick = () => {
     setIsVisible(false);
   };
 
+  const handleNewChatClick = (e) => {
+    e.preventDefault(); // Prevent the default anchor behavior
+    window.location.href = "/"; // Reload the page
+  };
+
   return (
-    <div className="App"> 
-      <Navbar />
-      <div className="main-content">
-        <h1>Jana Sahayi</h1>
-        
-        <div className="chat-container">
-        {isVisible && <h2>എനിക്ക് നിങ്ങളെ എങ്ങനെ സഹായിക്കാനാകും?</h2>}
-        {/* Pass the handleSendClick function to the ChatInput component */}
-          <ChatBox messages={messages} />
-          <ChatInput addMessage={addMessage} onSend={handleSendClick}/>
-        </div>
+    <div className="main-content">
+      <div className='header'>
+       <h1>ജന സഹായി</h1>
+       <Link to="/aboutus" className="aboutus circle-link" data-hover="About Us">
+        {/* <InfoIcon style={{ fontSize: 50 }} /> */}
+        ABOUT US
+      </Link>
       </div>
+      <div className="chat-container">
+        
+          {isVisible && <h2>എനിക്ക് നിങ്ങളെ എങ്ങനെ സഹായിക്കാനാകും?</h2>}
+          <ChatBox messages={messages} addFeedback={addFeedback} />
+          <div className='user-input'>
+          <ChatInput addMessage={addMessage} onSend={handleSendClick} />
+       
+       
+      
+      </div>
+     <Navbar/>
+      </div>
+      
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+        </Routes>
+       
+      </div>
+    </Router>
   );
 }
 
