5f19aed6febd1f57bba550470b78d02dad10d25a
M S Devanarayan <116883606+msdnarayan@users.noreply.github.com>
2024-07-01 21:06:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import InfoIcon from '@mui/icons-material/Info';
 
 const Navbar = () => {
   const handleNewChatClick = (e) => {
     e.preventDefault(); // Prevent the default anchor behavior
-    window.location.reload(); // Reload the page
+    window.location.href = "/"; // Reload the page
   };
 
   return (
     <div className="navbar">
-      <a href="#home" onClick={handleNewChatClick}>New Chat</a>
-      <div>
-        <a href="#home" className="aboutus">AboutUs</a>
-      </div>
+      <Link to="/" onClick={handleNewChatClick} className="circle-link" data-hover="പുതിയ ചാറ്റ്">
+        <AddCommentIcon style={{ fontSize: 45, color: '#007bff' }} />
+      </Link>
+      
     </div>
   );
 };
